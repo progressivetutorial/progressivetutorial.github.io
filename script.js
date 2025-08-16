953d1fa099c8543887ee6a4f6d5783c15a08b302
progressivetutorial <progressivetutorialbbsr@gmail.com>
2025-08-16 22:03:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   window.addEventListener("resize", updateCarousel);
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 
   updateCarousel();
